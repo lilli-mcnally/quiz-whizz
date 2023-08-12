68c11ac79467265522bea56eefab706446bcfa6f
Lilli McNally <Lilli.lawlor@hotmail.co.uk>
2023-08-12 20:51:08 +0000

    Update randomised answers function

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,11 +12,6 @@ async function getQuestions() {
 }
 
 let game = {
-    one: questions[index].correct_answer,
-    two: questions[index].incorrect_answers[0],
-    three: questions[index].incorrect_answers[1],
-    four: questions[index].incorrect_answers[2],
-    answers: [one, two, three, four],
     a: "",
     b: "",
     c: "",
@@ -25,74 +20,20 @@ let game = {
 
 function getNewQuestion() {
     document.getElementById("question").innerHTML = questions[index].question;
-    testFunction()
 
-    assignLetter(game.a)
-    assignLetter(game.b)
-    assignLetter(game.c)
-    assignLetter(game.d)
-    console.log(game.a)
-    console.log(game.b)
-    console.log(game.c)
-    console.log(game.d)
+    let one = questions[index].correct_answer;
+    let two = questions[index].incorrect_answers[0];
+    let three = questions[index].incorrect_answers[1];
+    let four = questions[index].incorrect_answers[2];
+    let answers = [one, two, three, four];
+
+    answer = answers.sort();
+
+    document.getElementById("answer-one").innerHTML = answers[0];
+    document.getElementById("answer-two").innerHTML = answers[1];
+    document.getElementById("answer-three").innerHTML = answers[2];
+    document.getElementById("answer-four").innerHTML = answers[3];
 }
-
-function assignLetter(number) {
-    number = game.answers[Math.floor(Math.random() * answers.length)]
-    return number
-}
-
-// function ryan() {
-//     if (4 < answerIndex.length) {
-//         testFunction()
-//     }
-// }
-
-// function testFunction() {
-//     // let y = Math.floor(Math.random() * 4)
-//     // answerIndex.push(y)
-//     // console.log(answerIndex);
-//     // for (i = 0; i < answerIndex.length; i++) {
-//     //     if (answerIndex.includes(y)) {
-//     //         testFunction()
-//     //     }}
-// }
-
-// function answerOne() {
-//     console.log(answers)
-//     let a = answers[Math.floor(Math.random() * answers.length)]
-//     return a;
-// }
-
-// function answerTwo() {
-//     let b = answers[Math.floor(Math.random() * answers.length)]
-//     if (b === a) {
-//         answerTwo();
-//     }
-//     return b;
-// }
-
-// console.log(a)
-// console.log(b)
-
-// let a = answers[Math.floor(Math.random() * answers.length)]
-// console.log(a);
-// let b = answers[Math.floor(Math.random() * answers.length)]
-
-// console.log(b);
-// let c = answers[Math.floor(Math.random() * answers.length)]
-// console.log(c);
-// let d = answers[Math.floor(Math.random() * answers.length)]
-// console.log(d);
-
-// document.getElementById("answer-one").innerHTML = a;
-// document.getElementById("answer-two").innerHTML = answers[Math.floor(Math.random() * answers.length)];
-// document.getElementById("answer-three").innerHTML = answers[Math.floor(Math.random() * answers.length)];
-// document.getElementById("answer-four").innerHTML = answers[Math.floor(Math.random() * answers.length)];
-
-
-
-
 
 function userSubmit() {
     index++;
@@ -108,25 +49,3 @@ async function main() {
 }
 
 main()
-
-// function getData(cb) {
-//     let api = new XMLHttpRequest();
-//     let data;
-
-//     api.open("GET", "https://opentdb.com/api.php?amount=50&category=11&type=multiple");
-//     api.send();
-
-//     api.onreadystatechange = function () {
-//         if (this.readyState == 4 && this.status == 200) {
-//             cb(JSON.parse(this.responseText));
-//         }
-//     };
-
-// }
-
-// function accessApi(data) {
-//     console.log(data);
-//     document.getElementById("test").innerHTML = data.results[0].question
-// }
-
-// getData(accessApi);
