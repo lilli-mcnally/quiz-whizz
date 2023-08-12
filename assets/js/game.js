8bac0d3ca5c9eb36621a21e65d624cfcaefa0c43
Lilli McNally <Lilli.lawlor@hotmail.co.uk>
2023-08-12 17:58:08 +0000

    Add functions for random answers

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,6 +3,7 @@ let q = params.get("q");
 let questions = []
 let index = 0;
 let submit = document.getElementById("submit");
+let answerIndex = [];
 
 async function getQuestions() {
     const response = await fetch(`https://opentdb.com/api.php?amount=${Number(q)}&category=11&type=multiple`)
@@ -10,19 +11,88 @@ async function getQuestions() {
     questions = json.results
 }
 
+let game = {
+    one: questions[index].correct_answer,
+    two: questions[index].incorrect_answers[0],
+    three: questions[index].incorrect_answers[1],
+    four: questions[index].incorrect_answers[2],
+    answers: [one, two, three, four],
+    a: "",
+    b: "",
+    c: "",
+    d: "",
+}
+
 function getNewQuestion() {
     document.getElementById("question").innerHTML = questions[index].question;
-    let one = questions[index].correct_answer;
-    let two = questions[index].incorrect_answers[0];
-    let three = questions[index].incorrect_answers[1];
-    let four = questions[index].incorrect_answers[2];
-    let answers = [one, two, three, four];
-    // console.log(answers);
+    testFunction()
 
-    let x = Math.random() * 10;
-    console.log(x)
-    // document.getElementById("answer-one").innerHTML = answers[x];
+    assignLetter(game.a)
+    assignLetter(game.b)
+    assignLetter(game.c)
+    assignLetter(game.d)
+    console.log(game.a)
+    console.log(game.b)
+    console.log(game.c)
+    console.log(game.d)
 }
+
+function assignLetter(number) {
+    number = game.answers[Math.floor(Math.random() * answers.length)]
+    return number
+}
+
+// function ryan() {
+//     if (4 < answerIndex.length) {
+//         testFunction()
+//     }
+// }
+
+// function testFunction() {
+//     // let y = Math.floor(Math.random() * 4)
+//     // answerIndex.push(y)
+//     // console.log(answerIndex);
+//     // for (i = 0; i < answerIndex.length; i++) {
+//     //     if (answerIndex.includes(y)) {
+//     //         testFunction()
+//     //     }}
+// }
+
+// function answerOne() {
+//     console.log(answers)
+//     let a = answers[Math.floor(Math.random() * answers.length)]
+//     return a;
+// }
+
+// function answerTwo() {
+//     let b = answers[Math.floor(Math.random() * answers.length)]
+//     if (b === a) {
+//         answerTwo();
+//     }
+//     return b;
+// }
+
+// console.log(a)
+// console.log(b)
+
+// let a = answers[Math.floor(Math.random() * answers.length)]
+// console.log(a);
+// let b = answers[Math.floor(Math.random() * answers.length)]
+
+// console.log(b);
+// let c = answers[Math.floor(Math.random() * answers.length)]
+// console.log(c);
+// let d = answers[Math.floor(Math.random() * answers.length)]
+// console.log(d);
+
+// document.getElementById("answer-one").innerHTML = a;
+// document.getElementById("answer-two").innerHTML = answers[Math.floor(Math.random() * answers.length)];
+// document.getElementById("answer-three").innerHTML = answers[Math.floor(Math.random() * answers.length)];
+// document.getElementById("answer-four").innerHTML = answers[Math.floor(Math.random() * answers.length)];
+
+
+
+
 
 function userSubmit() {
     index++;
