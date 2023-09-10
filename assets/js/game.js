bfbdf4b1aa5ae770d3995f04580eec7e4aff2594
Lilli McNally <Lilli.lawlor@hotmail.co.uk>
2023-09-10 12:53:45 +0000

    Add Modal to all pages

--- assets/js/game.js
+++ assets/js/game.js
@@ -148,21 +148,6 @@ function gameOver() {
     }
 }
 
-// An event listener that makes the Rules modal visible / invisible
-document.getElementById("rules-link").onclick = function seeRules() {
-    let rulesModal = document.getElementById("rules-modal");
-    let close = document.getElementById("close");
-
-    // Makes the Rules modal visible
-    rulesModal.style.display = "block";
-
-    //Makes the Rules modal invisible
-    close.onclick = function () {
-        rulesModal.style.display = "none";
-    }
-}
-
-
 async function main() {
     //Waits for the page to load, so Javascript can find the value of q before using it in the getQuestions function
     await getQuestions()
---
+++ assets/js/modal.js
@@ -0,0 +1,13 @@
+// An event listener that makes the Rules modal visible / invisible
+document.getElementById("rules-link").onclick = function seeRules() {
+    let rulesModal = document.getElementById("rules-modal");
+    let close = document.getElementById("close");
+
+    // Makes the Rules modal visible
+    rulesModal.style.display = "block";
+
+    //Makes the Rules modal invisible
+    close.onclick = function () {
+        rulesModal.style.display = "none";
+    }
+}
